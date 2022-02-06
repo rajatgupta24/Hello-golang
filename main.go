238adee338c6e3bf3072e9fa26eb8ee2cc5cbca5
rajat <rajat2411gupta@gmail.com>
2022-02-06 23:42:49 +0530

    reading the csv file

--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
-	// "fmt"
+	"fmt"
+	"os"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "The csv file contains all questions in format of question,answer")
 	flag.Parse()
-	_ = csvFilename
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open the CSV file %s\n", *csvFilename))
+	}
+
+	r := csv.NewReader(file)
+
+	lines, err := r.ReadAll()
+	if err != nil {
+		fmt.Printf("Failed to open the CSV file %s\n", *csvFilename)
+		os.Exit(1)
+	}
+	fmt.Println(lines)
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
